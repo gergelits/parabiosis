cf60f004b8c6941e8ae6c42dcf7e7eb583e117b6
Václav Gergelits <vaclav.gergelits@gmail.com>
2025-10-31 17:15:40 +0000

    update: fixed outdated model call, updated README, outdated header comments - 4

--- script/setup_simulation_parameters_i20006.r
+++ script/setup_simulation_parameters_i20006.r
@@ -1,4 +1,4 @@
-# setup_simulation_parameters_i5004.r
+# setup_simulation_parameters_i20006.r
 
 seed.base <- 20230101
 seed <- set_seed_here_visible( seed.base, "markov model" )
@@ -9,3 +9,4 @@ mcmc.core.n <- mcmc.chain.n
 sampling.adapt_delta <- 0.50
 options( mc.cores = mcmc.core.n )
 rstan_options( auto_write = TRUE )
+
